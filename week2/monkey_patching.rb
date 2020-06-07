0b2f5a098a41c1596470b893d16b5804a1079e30
smarthojin <smarthojin@gmail.com>
2020-06-07 16:28:22 -0700

    monkey_patching
    a

--- week2/monkey_patching.rb
+++ week2/monkey_patching.rb
@@ -42,6 +42,7 @@
 #      3
 
 # p return_value  # => [1, 2, 3]
+
 # My Select
 # Now extend the Array class to include my_select that takes a block and returns a new array containing only elements 
 # that satisfy the block. Use your my_each method!
@@ -51,6 +52,8 @@
 # a = [1, 2, 3]
 # a.my_select { |num| num > 1 } # => [2, 3]
 # a.my_select { |num| num == 4 } # => []
+
+
 # My Reject
 # Write my_reject to take a block and return a new array excluding elements that satisfy the block.
 
--- week2/monkey_patching_solutions.rb
+++ week2/monkey_patching_solutions.rb
@@ -1,6 +1,6 @@
 class Array
   
-  def My_Each(&block)
+  def my_each(&block)
     self.length.times do |i|
       block.call(self[i])
     end
@@ -8,18 +8,56 @@ class Array
     self
 
   end
+ 
+  def my_select(&block)  
+    results = []
+    self.my_each do |i|
+      results.push(i) if block.call(i)
+    end 
+    results
+  end
 
+  def my_reject(&block)
+    results = []
+    self.my_each do |i|
+      results.push(i) unless block.call(i)
+    end
+    results
+  end
 
+  def my_any?(&block)
+    self.my_each do |i|
+      return true if block.call(i)
+    end
+    false
+  end
+    
 
 
 end
 
 
-
-def My_Each(array, &block)
+def nc_my_each(array, &block)
   array.length.times do |i|
     block.call(array[i])
   end
   array
 end
 
+def nc_my_select(array, &block)
+  results = []
+  array.my_each do |i|
+    results.push(i) if block.call(i)
+  end
+  results
+end
+
+def nc_my_reject(array, &block)
+  results = []
+  array.my_each do |i|
+    result.push (i) unless block.call{i}
+  end
+  results
+end
+
+array.my_any?{ |ele| ele.is_a? String}
