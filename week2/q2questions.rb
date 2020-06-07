19108f40fcc0decf480d58b5c72c4a24c9f00efe
smarthojin <smarthojin@gmail.com>
2020-06-07 14:49:51 -0700

    each_cons

--- week2/q2questions.rb
+++ week2/q2questions.rb
@@ -4,17 +4,54 @@
 #   digits.chars.each_cons(5).max_by(&:itself).join.to_i
 # end
 
+
+
 # whats max_by(&:itself)?
 
 
-
 # def solution(digits)
-#   digits.chars.each_cons(5).map{|arr| arr.join.to_i}.max
+#   digits.chars.each_cons(3).map{|arr| arr.join.to_i}.max
+ digits '134679'
+ digits.chars = ['1', '3', '4', '6', '7', '9']
+#  digits.split("") = 
+ digits.chars.(3) =  [[346],[467],[679]]
+ digits.chars.each_cons(3).map{|arr| arr.join.to_i} = 123,346,467,679
 # end
 
 #why loop twice with each and map?
 
+def ho_each_cons(array, num)
+  result = []
+  
+  for i in 0...(array.length - num) do
+    result.push(array[i...(i+num)])
+  end
+  result
+end
 
+
+
+    # [1, 2, 3, 4, 5, 6, 7, 8]
+    #  2
+
+ho_each_cons
+# each_cons(n) { ... } → nil
+# each_cons(n) → an_enumerator
+# Iterates the given block for each array of consecutive <n> elements. 
+# If no block is given, returns an enumerator.
+
+# e.g.:
+
+# (1..10).each_cons(3) { |a| p a }
+# outputs below
+[1, 2, 3]
+[2, 3, 4]
+[3, 4, 5]
+[4, 5, 6]
+[5, 6, 7]
+[6, 7, 8]
+[7, 8, 9]
+[8, 9, 10]
 
 # def solution(digits)
 #   result = 0
